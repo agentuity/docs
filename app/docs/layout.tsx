8203feb9ba5e60473b06712e91d7ed5f49dc3e86
Matthew Congrove <mattcongrove@gmail.com>
2025-03-03 14:15:50 -0600

    UI tweaks

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -1,7 +1,7 @@
-import { DocsLayout } from "fumadocs-ui/layouts/notebook";
-import type { ReactNode } from "react";
 import { baseOptions } from "@/app/layout.config";
 import { source } from "@/lib/source";
+import { DocsLayout } from "fumadocs-ui/layouts/notebook";
+import type { ReactNode } from "react";
 // import { RootToggle } from "fumadocs-ui/components/layout/root-toggle";
 
 export default function Layout({ children }: { children: ReactNode }) {
