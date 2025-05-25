c7d39c65d0112c5eb51a7cabdd147a90c9a5648d
Jeff Haynie <jhaynie@gmail.com>
2025-05-24 20:52:43 -0500

    fix theme when using system default

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,16 @@
 import { RootProvider } from "fumadocs-ui/provider";
 import { GeistMono } from "geist/font/mono";
 import type { ReactNode } from "react";
+
 import "./global.css";
 
 export default function Layout({ children }: { children: ReactNode }) {
 	return (
 		<html lang="en" className={GeistMono.className} suppressHydrationWarning>
 			<body className="flex flex-col min-h-screen antialiased">
-				<RootProvider>{children}</RootProvider>
+				<RootProvider theme={{ enabled: true, enableSystem: true }}>
+					{children}
+				</RootProvider>
 			</body>
 		</html>
 	);
--- components/ThemeImage.tsx
+++ components/ThemeImage.tsx
@@ -5,13 +5,16 @@ import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
 import ImageZoom from "react-medium-image-zoom";
 
+const placeholder =
+	"data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7";
+
 export function ThemeImage({
 	baseName,
 	alt,
 	width = 3680,
 	height = 2382,
 }: { baseName: string; alt: string; width: number; height: number }) {
-	const { theme } = useTheme();
+	const { theme, resolvedTheme } = useTheme();
 	const [loaded, setLoaded] = useState(false);
 	const [src, setSrc] = useState<string | null>(null);
 
@@ -20,15 +23,15 @@ export function ThemeImage({
 	}, []);
 
 	useEffect(() => {
-		if (theme === "dark") {
+		if (theme === "dark" || (theme === "system" && resolvedTheme === "dark")) {
 			setSrc(`/images/${baseName}-dark.png`);
 		} else {
 			setSrc(`/images/${baseName}-light.png`);
 		}
-	}, [theme, baseName]);
+	}, [theme, resolvedTheme, baseName]);
 
 	if (!baseName || !loaded || !src) {
-		return null;
+		return <img alt={alt} src={placeholder} width={width} height={height} />;
 	}
 
 	return (
