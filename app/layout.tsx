4efdd87b366407ba83a57da7f13de94f697b824d
Jeff Haynie <jhaynie@gmail.com>
2025-05-24 21:05:53 -0500

    small tweaks

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import { RootProvider } from "fumadocs-ui/provider";
 import { GeistMono } from "geist/font/mono";
 import type { ReactNode } from "react";
-
 import "./global.css";
 
 export default function Layout({ children }: { children: ReactNode }) {
